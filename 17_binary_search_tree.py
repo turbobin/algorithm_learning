44cd907303383c5c119635d1dce39c74a57a6f4b
turbobin <turbobin.cao@gmail.com>
2020-10-14 20:51:01 +0800

    update file

--- 17_binary_search_tree.py
+++ 17_binary_search_tree.py
@@ -6,6 +6,9 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return "TreeNode({})".format(self.val)
+
 
 class BinarySearchTree:
     def __init__(self, val=None):
@@ -14,9 +17,13 @@ class BinarySearchTree:
     def find(self, val):
         node = self.root
         # 从根节点开始比较每个节点大小
+        path = ""
         while node and node.val != val:
+            path += "{}->".format(node)
             parent = node
             node = parent.left if val < parent.val else parent.right
+        path += str(node)
+        print(path)
         return node if node.val == val else None
 
     def insert(self, val):
@@ -38,7 +45,7 @@ class BinarySearchTree:
     def delete(self, val):
         # 先查找到这个节点
         node = self.root
-        while node and node.val != val:
+        while node.val and node.val != val:
             parent = node
             node = parent.left if val < parent.val else parent.right
         if not node:
@@ -53,54 +60,95 @@ class BinarySearchTree:
                 parent.right = child
             else:
                 self.root = None
+        else:
         # 要删除的节点有两个节点，先在节点右子树中查找到最小节点
-        min_node = node.right
-        min_node_parent = node
-        while min_node.left:
-            min_node_parent = min_node
-            min_node = min_node_parent.left
-        # 将最小节点替换到被删除节点上
-        node.val, min_node.val = min_node.val, node.val
-        # 删除这个最小节点
-        if min_node_parent.right == min_node:
-            # 考虑没有左子节点的情况
-            min_node_parent.right = min_node.right
-        elif min_node_parent.left == min_node:
-            min_node_parent.left = None
+            min_node = node.right
+            min_node_parent = node
+            while min_node.left:
+                min_node_parent = min_node
+                min_node = min_node_parent.left
+            # 将最小节点替换到被删除节点上
+            node.val, min_node.val = min_node.val, node.val
+            # 删除这个最小节点
+            if min_node_parent.right == min_node:
+                # 考虑没有左子节点的情况
+                min_node_parent.right = min_node.right
+            elif min_node_parent.left == min_node:
+                min_node_parent.left = None
 
-    def print_tree(self, root):
-        from collections import deque
-        res = []
-        q = deque([root])
-        while q:
-            row = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                if not node:
-                    row.append(" ")
-                    continue
-                row.append(node.val)
-                q.append(node.left)
-                q.append(node.right)
-            res.append(row)
-        rows = len(res)
-        print(res)
-        base = 2**(rows)
-        for r in range(rows):
-            for v in res[r]:
-                print(" " * (base), end = "")
-                print(v, end = "")
-                print(" " * (base - 1), end = "")
-            print("")
-            base //= 2
+    def _build_tree_string(self, root, curr_index, index=False, delimiter='-'):
+        if root is None:
+            return [], 0, 0, 0
+
+        line1 = []
+        line2 = []
+        if index:
+            node_repr = '{}{}{}'.format(curr_index, delimiter, root.val)
+        else:
+            node_repr = str(root.val)
+
+        new_root_width = gap_size = len(node_repr)
+
+        # Get the left and right sub-boxes, their widths, and root repr positions
+        l_box, l_box_width, l_root_start, l_root_end = \
+            self._build_tree_string(root.left, 2 * curr_index + 1, index, delimiter)
+        r_box, r_box_width, r_root_start, r_root_end = \
+            self._build_tree_string(root.right, 2 * curr_index + 2, index, delimiter)
+
+        # Draw the branch connecting the current root node to the left sub-box
+        # Pad the line with whitespaces where necessary
+        if l_box_width > 0:
+            l_root = (l_root_start + l_root_end) // 2 + 1
+            line1.append(' ' * (l_root + 1))
+            line1.append('_' * (l_box_width - l_root))
+            line2.append(' ' * l_root + '/')
+            line2.append(' ' * (l_box_width - l_root))
+            new_root_start = l_box_width + 1
+            gap_size += 1
+        else:
+            new_root_start = 0
+
+        # Draw the representation of the current root node
+        line1.append(node_repr)
+        line2.append(' ' * new_root_width)
+
+        # Draw the branch connecting the current root node to the right sub-box
+        # Pad the line with whitespaces where necessary
+        if r_box_width > 0:
+            r_root = (r_root_start + r_root_end) // 2
+            line1.append('_' * r_root)
+            line1.append(' ' * (r_box_width - r_root + 1))
+            line2.append(' ' * r_root + '\\')
+            line2.append(' ' * (r_box_width - r_root))
+            gap_size += 1
+        new_root_end = new_root_start + new_root_width - 1
+
+        # Combine the left and right sub-boxes with the branches drawn above
+        gap = ' ' * gap_size
+        new_box = [''.join(line1), ''.join(line2)]
+        for i in range(max(len(l_box), len(r_box))):
+            l_line = l_box[i] if i < len(l_box) else ' ' * l_box_width
+            r_line = r_box[i] if i < len(r_box) else ' ' * r_box_width
+            new_box.append(l_line + gap + r_line)
+
+        # Return the new box, its width and its root repr positions
+        return new_box, len(new_box[0]), new_root_start, new_root_end
+
+    def __repr__(self):
+        lines = self._build_tree_string(self.root, 0, False, '-')[0]
+        return '\n' + '\n'.join((line.rstrip() for line in lines))
 
 
 if __name__ == '__main__':
     bst = BinarySearchTree()
-    bst.insert(3)
+    bst.insert(4)
     bst.insert(2)
-    bst.insert(5)
-    bst.insert(7)
+    bst.insert(1)
+    bst.insert(3)
     bst.insert(6)
+    bst.insert(5)
     bst.insert(8)
-    bst.print_tree(bst.root)
+    bst.insert(7)
+    # bst.delete(8)
+    print(bst.find(7))
+    print(bst)
